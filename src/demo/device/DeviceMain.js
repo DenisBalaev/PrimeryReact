9a2aec0d63ef9ed229ea0a755097353931aa4e70
Denis <balaev.denis2002@gmail.com>
2023-03-19 10:56:30 +0300

    Добавление функции удаления

--- src/demo/device/DeviceMain.js
+++ src/demo/device/DeviceMain.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef  } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { DeviceService } from '../../service/DeviceService';
@@ -10,6 +10,7 @@ import { Dialog } from 'primereact/dialog';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { RadioButton } from 'primereact/radiobutton';
 import { InputText } from 'primereact/inputtext';
+import { Toast } from 'primereact/toast';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.css';
@@ -30,11 +31,15 @@ export default function DeviceMain() {
         summDay:0
     };
 
+    const toast = useRef(null);
     const [devices, setDevices] = useState(null);
     const [selectedDevices, setSelectedDevices] = useState(null);
     const [device, setDevice] = useState(emptyDevice);
     const [submitted, setSubmitted] = useState(false);
     const [deviceDialog, setDeviceDialog] = useState(false);
+    const [deleteDevicesDialog, setDeleteDevicesDialog] = useState(false);
+    const [deleteDeviceDialog, setDeleteDeviceDialog] = useState(false);
+
 
 
     useEffect(() => {
@@ -90,13 +95,72 @@ export default function DeviceMain() {
       };
 
       const confirmDeleteSelected = () => {
-
+        setDeleteDevicesDialog(true);
       };
+
+      const hideDeleteDevicesDialog = () => {
+        setDeleteDevicesDialog(false);
+      };
+
+      const hideDeleteDeviceDialog = () => {
+        setDeleteDeviceDialog(false);
+      };
+
+      const deleteSelectedDevices = () => {
+        let _devices = devices.filter((val) => !selectedDevices.includes(val));
+
+        setDevices(_devices);
+        setDeleteDevicesDialog(false);
+        setSelectedDevices(null);
+        toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
+      };
+
+      const deleteProduct = () => {
+        let _devices = devices.filter((val) => val.id !== device.id);
+
+        setDevices(_devices);
+        setDeleteDeviceDialog(false);
+        setDevice(emptyDevice);
+        toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
+    };
+
+    const confirmDeleteDevice = (device) => {
+      setDevice(device);
+      setDeleteDeviceDialog(true);
+    };
+
+    const editProduct = (device) => {
+      setDevice({ ...device });
+      setDeviceDialog(true);
+    };
+
+    const actionBodyTemplate = (rowData) => {
+      return (
+          <React.Fragment>
+              <Button icon="pi pi-pencil" rounded outlined className="mr-2" onClick={() => editProduct(rowData)} />
+              <Button icon="pi pi-trash" rounded outlined severity="danger" onClick={() => confirmDeleteDevice(rowData)} />
+          </React.Fragment>
+      );
+    };
+
+      const deleteDeviceDialogFooter = (
+        <React.Fragment>
+            <Button label="No" icon="pi pi-times" outlined onClick={hideDeleteDeviceDialog} />
+            <Button label="Yes" icon="pi pi-check" severity="danger" onClick={deleteProduct} />
+        </React.Fragment>
+      );
+      const deleteDevicesDialogFooter = (
+          <React.Fragment>
+              <Button label="No" icon="pi pi-times" outlined onClick={hideDeleteDevicesDialog} />
+              <Button label="Yes" icon="pi pi-check" severity="danger" onClick={deleteSelectedDevices} />
+          </React.Fragment>
+      );
+
       
       return (
         <div>
           <div className="card">
-
+          <Toast ref={toast} />
             <Toolbar className="mb-4" left={leftToolbarTemplate}/>
             <DataTable value={devices} selection={selectedDevices} onSelectionChange={(e) => setSelectedDevices(e.value)} 
             headerColumnGroup={headerGroup} scrollable scrollHeight="100%" sortField="dateAdded" sortOrder={-1}>
@@ -112,6 +176,7 @@ export default function DeviceMain() {
                 <Column field="dateAdded" header="Дата добавления в админку" body={dateAddedBody}></Column>
                 <Column field="summMo" header="Сумма продаж за последний месяц"></Column>
                 <Column field="summDay" header="Сумма продаж за день"></Column>
+                <Column body={actionBodyTemplate} exportable={false} style={{ minWidth: '12rem' }}></Column>
                 
             </DataTable>
 
@@ -135,6 +200,24 @@ export default function DeviceMain() {
                 <div className="field">
                 </div>
 
+            </Dialog>
+
+            <Dialog visible={deleteDevicesDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Confirm" modal footer={deleteDevicesDialogFooter} onHide={hideDeleteDevicesDialog}>
+                <div className="confirmation-content">
+                    <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
+                    {device && <span>Are you sure you want to delete the selected products?</span>}
+                </div>
+            </Dialog>
+
+            <Dialog visible={deleteDeviceDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Confirm" modal footer={deleteDeviceDialogFooter} onHide={hideDeleteDeviceDialog}>
+                <div className="confirmation-content">
+                    <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
+                    {device && (
+                        <span>
+                            Are you sure you want to delete <b>{device.id}</b>?
+                        </span>
+                    )}
+                </div>
             </Dialog>
 
           </div>
